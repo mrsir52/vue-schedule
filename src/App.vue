67939af9b830c6e43279650e0b19a1b01e530455
Manny <coachmannytw@gmail.com>
2019-11-24 22:52:42 -0700

    vue schedule tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <mdb-container>
+    <mdb-row>
+      <mdb-col col="9">
+        <h2 class="text-uppercase my-3">Today:</h2>
+        <Event
+          v-for="(event, index) in events"
+          :index="index"
+          :time="event.time"
+          :title="event.title"
+          :location="event.location"
+          :description="event.description"
+          :key="index"
+        />
+        <mdb-row>
+          <mdb-col xl="3" md="6" class="mx-auto text-center">
+            <mdb-btn color="info">Add Event</mdb-btn>
+          </mdb-col>
+        </mdb-row>
+      </mdb-col>
+      <mdb-col col="3">
+        <h3 class="text-uppercase my-3">Schedule</h3>
+        <h6 class="my-3">
+          It's going to be busy that today. You have
+          <b>{{events.length}} events</b> today.
+        </h6>
+        <h1 class="my-3">
+          <mdb-row>
+            <mdb-col col="3" class="text-center">
+              <mdb-icon far icon="sun"/>
+            </mdb-col>
+            <mdb-col col="9">Sunny</mdb-col>
+          </mdb-row>
+          <mdb-row>
+            <mdb-col col="3" class="text-center">
+              <mdb-icon icon="thermometer-three-quarters"/>
+            </mdb-col>
+            <mdb-col col="9">23°C</mdb-col>
+          </mdb-row>
+        </h1>
+        <p>
+          Don't forget your sunglasses. Today will dry and sunny, becoming
+          warm in the afternoon with temperatures of between 20 and 25
+          degrees.
+        </p>
+      </mdb-col>
+    </mdb-row>
+  </mdb-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mdbContainer, mdbRow, mdbCol, mdbIcon, mdbBtn, } from "mdbvue";
+import Event from "@/components/Event";
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
+    mdbContainer,
+    mdbRow,
+    mdbCol,
+    Event,
+    mdbIcon,
+    mdbBtn,
+  },
+  data() {
+    return {
+      events: [
+        {
+          time: "10:00",
+          title: "Breakfast with Simon",
+          location: "Lounge Caffe",
+          description: "Discuss Q3 targets"
+        },
+        {
+          time: "10:30",
+          title: "Daily Standup Meeting (recurring)",
+          location: "Warsaw Spire Office"
+        },
+        {
+          time: "11:00",
+          title: "Call with HRs"
+        },
+        {
+          time: "12:00",
+          title: "Lunch with Timmoty",
+          location: "Canteen",
+          description: "Project evalutation"
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+div[class^="col"],
+div[class*=" col"] {
+  border: 1px dotted black;
 }
 </style>
